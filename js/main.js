d930600d56d79b2e2deedd6b1d5c149d31d19c5d
People34 <toha1997890@gmail.com>
2022-07-12 12:54:01 +0300

    add slick

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-$('.header__slider-inner').slick({
+$(function () {
+$('.header__slider').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToShow: 1,
+    slidesToScroll: 1,
   });
+
+})
